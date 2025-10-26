e4f6a3bb5b905e42cfedd0b063436180e197679e
Sergio Smirnoff <ssmirnoff@itba.edu.ar>
2025-10-25 23:31:20 -0300

    some changes Client.java

--- api/src/main/java/ar/edu/itba/pod/hazelcast/common/ZonesRow.java
+++ api/src/main/java/ar/edu/itba/pod/hazelcast/common/ZonesRow.java
@@ -1,4 +1,26 @@
 package ar.edu.itba.pod.hazelcast.common;
 
 public class ZonesRow {
+
+    private int LocationID;
+    private String Borogh;
+    private String Zone;
+
+    public ZonesRow(int locationID, String borogh, String zone) {
+        LocationID = locationID;
+        Borogh = borogh;
+        Zone = zone;
+    }
+
+    public int getLocationID() {
+        return LocationID;
+    }
+
+    public String getBorogh() {
+        return Borogh;
+    }
+
+    public String getZone() {
+        return Zone;
+    }
 }
--- client/src/main/java/ar/edu/itba/pod/hazelcast/client/Client.java
+++ client/src/main/java/ar/edu/itba/pod/hazelcast/client/Client.java
@@ -13,12 +13,13 @@ import com.hazelcast.mapreduce.KeyValueSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import ar.edu.itba.pod.hazelcast.common.*;
+
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
 public class Client {
@@ -29,7 +30,7 @@ public class Client {
         logger.info("grpc-com-patterns Client Starting ...");
         try {
             // Group Config
-            GroupConfig groupConfig = new GroupConfig().setName("l12345").setPassword("l12345-pass");
+            GroupConfig groupConfig = new GroupConfig().setName("g05-hazelcast").setPassword("g05-hazelcast-pass");
 
             // Client Network Config
             ClientNetworkConfig clientNetworkConfig = new ClientNetworkConfig();
@@ -41,30 +42,39 @@ public class Client {
             // Node Client
             HazelcastInstance hazelcastInstance = HazelcastClient.newHazelcastClient(clientConfig);
 
+
+            // ======================== Query 3 =============================
+            // Tiene doble map and reduce porque primero tiene que filtrar los Borough que si van y luego hacer el avg de precio
             // Key Value Source
-            MultiMap<String, BikeRentalRow> rentalsMultiMap = hazelcastInstance.getMultiMap("rentalsByStartStation");
-            KeyValueSource<String, BikeRentalRow> rentalsKeyValueSource = KeyValueSource.fromMultiMap(rentalsMultiMap);
+            MultiMap<Integer, TripRow> tripMultiMap = hazelcastInstance.getMultiMap("avgPriceByZoneAndComp");
+            KeyValueSource<Integer, TripRow> tripKeyValueSource = KeyValueSource.fromMultiMap(tripMultiMap);
 
             // Job Tracker
-            JobTracker jobTracker = hazelcastInstance.getJobTracker("bikeRentals");
+            JobTracker jobTracker = hazelcastInstance.getJobTracker("avgPriceByZoneAndComp");
+
+            // ======================== End Query 3 =============================
 
             // Text File Reading and Key Value Source Loading
             DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
             try (Stream<String> lines = Files.lines(Paths.get("client/src/main/assembly/trips-2025-01.csv"), StandardCharsets.UTF_8)) {
                 lines.skip(1)
                         .map(line -> line.split(";"))
-                        .map(line -> new BikeRentalRow(
+                        .map(line -> new TripRow(
                                 line[0],
                                 LocalDateTime.parse(line[1], dateTimeFormatter),
-                                line[2],
+                                LocalDateTime.parse(line[2], dateTimeFormatter),
                                 LocalDateTime.parse(line[3], dateTimeFormatter),
-                                Integer.parseInt(line[4]) == 1))
-                        .forEach(bikeRental -> rentalsMultiMap.put(bikeRental.getStartStation(), bikeRental));
+                                Integer.parseInt(line[4]),
+                                Integer.parseInt(line[5]),
+                                Double.parseDouble(line[6]),
+                                Double.parseDouble(line[7])
+                                ))
+                        .forEach(trip -> tripMultiMap.put(trip.getPULocationID(), trip));
             }
 
             // Ejercicio 2.1
             // Check how many objects where loaded
-            System.out.println(rentalsMultiMap.size());
+            System.out.println(tripMultiMap.size());
         } finally {
             HazelcastClient.shutdownAll();
         }
