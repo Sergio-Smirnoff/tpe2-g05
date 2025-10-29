3fd06343ee7ded4c806983bce940f348f8063a82
LucasCampoli <112361327+LucasCampoli@users.noreply.github.com>
2025-10-29 09:25:52 -0300

    Solve error on query5

--- api/src/main/java/ar/edu/itba/pod/hazelcast/query5/TotalMilesMapper.java
+++ api/src/main/java/ar/edu/itba/pod/hazelcast/query5/TotalMilesMapper.java
@@ -10,8 +10,6 @@ import com.hazelcast.mapreduce.Mapper;
 import java.util.Set;
 
 public class TotalMilesMapper implements Mapper<Integer, TripRow, TotalMilesKey, Double> {
-    private static final Long ONE = 1L;
-
     @Override
     public void map(Integer integer, TripRow tripRow, Context<TotalMilesKey, Double> context) {
         TotalMilesKey key = new TotalMilesKey(tripRow.getCompany(), tripRow.getRequestTime().getYear(), tripRow.getRequestTime().getMonthValue());
