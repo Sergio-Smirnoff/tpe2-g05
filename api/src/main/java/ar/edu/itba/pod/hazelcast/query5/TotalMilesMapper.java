2a4240e79e8cf3a220570bdb15fea99f5b6d5e3e
martuschvartz <schvartzmartina@gmail.com>
2025-10-29 16:06:01 -0300

    removed old class imports

--- api/src/main/java/ar/edu/itba/pod/hazelcast/query5/TotalMilesMapper.java
+++ api/src/main/java/ar/edu/itba/pod/hazelcast/query5/TotalMilesMapper.java
@@ -1,13 +1,9 @@
 package ar.edu.itba.pod.hazelcast.query5;
 
 import ar.edu.itba.pod.hazelcast.common.TripRow;
-import ar.edu.itba.pod.hazelcast.query1.StartEndPair;
-import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.HazelcastInstanceAware;
+
 import com.hazelcast.mapreduce.Context;
 import com.hazelcast.mapreduce.Mapper;
-
-import java.util.Set;
 
 public class TotalMilesMapper implements Mapper<Integer, TripRow, TotalMilesKey, Double> {
     @Override
