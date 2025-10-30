926d9439150b40c4cc58e6181fce0f11ca595ed4
LucasCampoli <112361327+LucasCampoli@users.noreply.github.com>
2025-10-29 21:50:51 -0300

    Implement DataSerializable

--- api/src/main/java/ar/edu/itba/pod/hazelcast/query5/objects/TotalMilesKey.java
+++ api/src/main/java/ar/edu/itba/pod/hazelcast/query5/objects/TotalMilesKey.java
@@ -1,18 +1,59 @@
 package ar.edu.itba.pod.hazelcast.query5.objects;
 
-import java.io.Serializable;
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
+import com.hazelcast.nio.serialization.DataSerializable;
+
+import java.io.IOException;
 import java.util.Objects;
 
-public record TotalMilesKey (String company, int year, int month) implements Serializable {
+public class TotalMilesKey implements DataSerializable {
+    private String company;
+    private int year;
+    private int month;
+
+    public TotalMilesKey() { }
+
+    public TotalMilesKey(String company, int year, int month) {
+        this.company = company;
+        this.year = year;
+        this.month = month;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        TotalMilesKey that = (TotalMilesKey) o;
-        return year == that.year && month == that.month && Objects.equals(company, that.company);
+        return o instanceof TotalMilesKey totalMilesKey
+                && year == totalMilesKey.year && month == totalMilesKey.month && company.equals(totalMilesKey.company);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(company, year, month);
+    }
+
+    @Override
+    public void writeData(ObjectDataOutput out) throws IOException {
+        out.writeUTF(company);
+        out.writeInt(year);
+        out.writeInt(month);
+    }
+
+    @Override
+    public void readData(ObjectDataInput in) throws IOException {
+        company = in.readUTF();
+        year = in.readInt();
+        month = in.readInt();
     }
 }
