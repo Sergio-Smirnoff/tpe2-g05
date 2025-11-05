0e991f080173782a2b11bf6b535c3aa8c419169f
Sergio Smirnoff <ssmirnoff@itba.edu.ar>
2025-11-05 11:12:02 -0300

    flags de server

--- server.sh
+++ server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export JAVA_RUN_CLASS="Server"
+export JAVA_OPTS="-Xms3G -Xmx4G"
 
 # Verificar que las clases estén compiladas
 if [ ! -d "client/target/classes" ]; then
@@ -9,11 +10,12 @@ if [ ! -d "client/target/classes" ]; then
 fi
 
 cd server/target/tpe2-g05-server-2025.2Q
-for (( i=0; i<3; i++ ))
+for (( i=0; i<2; i++ ))
 do
   echo "--- Starting server (run $((i+1))) ---"
-  . ./run-server.sh $* &
+  . ./run-server.sh &
 done
 wait
 cd -
 unset JAVA_RUN_CLASS
+unset JAVA_OPTS
