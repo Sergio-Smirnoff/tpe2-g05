f869d84c7eb5a57abadb81ccfab09887629b826d
LucasCampoli <112361327+LucasCampoli@users.noreply.github.com>
2025-11-06 13:43:43 -0300

    Fix format

--- api/src/main/java/ar/edu/itba/pod/hazelcast/query5/objects/TotalMilesResult.java
+++ api/src/main/java/ar/edu/itba/pod/hazelcast/query5/objects/TotalMilesResult.java
@@ -5,6 +5,6 @@ package ar.edu.itba.pod.hazelcast.query5.objects;
 public record TotalMilesResult(String company, int year, int month, double totalMiles) {
     @Override
     public String toString() {
-        return "%s,%d,%d,%.2f".formatted(company, year, month, totalMiles);
+        return "%s;%d;%d;%.2f".formatted(company, year, month, totalMiles);
     }
 }
