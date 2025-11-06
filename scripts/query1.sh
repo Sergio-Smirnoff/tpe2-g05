9dfd5cb048fd9ba3b69d8a8b542d0f82744a2cfd
LucasCampoli <112361327+LucasCampoli@users.noreply.github.com>
2025-11-06 14:26:48 -0300

    Organize scripts

---
+++ scripts/query1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export JAVA_RUN_CLASS="ClientQuery1"
+
+# Verificar que las clases estén compiladas
+if [ ! -d "../client/target/classes" ]; then
+    echo "Classes not found. Run 'mvn package' first."
+    exit 1
+fi
+
+CLIENT_DIR="../client/target/tpe2-g05-client-2025.2Q"
+
+chmod +x "$CLIENT_DIR/run-client.sh"
+cd "$CLIENT_DIR"
+./run-client.sh $*
+cd -
+unset JAVA_RUN_CLASS
